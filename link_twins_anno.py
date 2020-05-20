8b86164e5861dce4391a0a118a81905bbac38d16
Simon Couvreur <simoncouvreur@hotmai.com>
2020-05-20 15:28:31 +0100

    link IDs for test results as well

--- link_twins_anno.py
+++ link_twins_anno.py
@@ -5,17 +5,22 @@ p_path = sys.argv[1]
 a_path = sys.argv[2]
 pl_path = sys.argv[3]
 al_path = sys.argv[4]
-mapfile = sys.argv[5]
+ct_path = sys.argv[5]
+ctl_path = sys.argv[6]
+mapfile = sys.argv[7]
 
 a = pd.read_csv(a_path, low_memory=False)
 p = pd.read_csv(p_path, low_memory=False)
+ct = pd.read_csv(ct_path, low_memory=False)
 id_map = pd.read_csv(mapfile)
 
 
 al = a.merge(id_map, left_on="patient_id", right_on="App_ID", how="inner")
 pl = p.merge(id_map, left_on="id", right_on="App_ID", how="inner")
+ctl = ct.merge(id_map, left_on="patient_id", right_on="App_ID", how="inner")
 
 al.to_csv(al_path, index=False)
 pl.to_csv(pl_path, index=False)
+ctl.to_csv(ctl_path, index=False)
 
 
